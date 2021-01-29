98488bfc232c0be9de5cfd08cfb44f8af0221a08
amir <olempico@gmail.com>
2021-01-29 23:02:49 +0330

    Implements nav component.

---
+++ app/src/main/java/com/sevenpeakssoftware/amirnaghavi/presentation/NavigationActivity.kt
@@ -0,0 +1,26 @@
+package com.sevenpeakssoftware.amirnaghavi.presentation
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import dagger.android.support.DaggerAppCompatActivity
+
+class NavigationActivity : DaggerAppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        makeFullScreen()
+
+    }
+
+    private fun makeFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.apply {
+            hide()
+        }
+    }
+}
